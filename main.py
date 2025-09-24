dfaaca12f02bd5ecfc72e7a271037a8280155b14
Volodymyr Vladykin <vova@pocketguard.com>
2025-09-24 23:27:20 +0300

    Refactor to use ArgumentParser; Install async libs

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import sys
+import asyncio
+from aiofile import async_open
+from aiopath import AsyncPath
 
 
-def main(source_dir_path: str, output_dir_path: str):
-    print(source_dir_path, output_dir_path)
+async def main(source_dir_path: str, output_dir_path: str):
+    apath = AsyncPath(source_dir_path)
+    print(f"Checking is ${source_dir_path} exists...")
+    print(await apath.exists())
+
+
+class ArgumentParser:
+    def parse_dir_paths(self, argv):
+        # first arg after script name is source dir, and second one is output dir
+        if len(argv) > 2:
+            source_dir_path = argv[1]
+            output_dir_path = argv[2]
+            return source_dir_path, output_dir_path
+        else:
+            raise Exception()
 
 
 if __name__ == "__main__":
-    # first arg after script name is source dir, and second one is output dir
-    if len(sys.argv) > 2:
-        source_dir_path = sys.argv[1]
-        output_dir_path = sys.argv[2]
-        main(source_dir_path, output_dir_path)
-    else:
+    try:
+        source_dir_path, output_dir_path = ArgumentParser().parse_dir_paths(sys.argv)
+        asyncio.run(main(source_dir_path, output_dir_path))
+    except:
         print(
             "Invalid parameters. Make sure to put path to source directory as first parameter of this script, and path to target directory as the second one"
         )
